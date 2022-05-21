896212ba0df4c7b4e5fd7c8ae3e76b2d3fa0efc8
sustovvladimir <kapusta1986@mail.ru>
2022-05-21 12:57:17 +0600

    refactor code

--- app/src/main/java/bi/group/nsi_app/ui/home/HomeViewModel.kt
+++ app/src/main/java/bi/group/nsi_app/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Это тестовый фрагмент для изучения"
     }
     val text: LiveData<String> = _text
 }
