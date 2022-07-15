a9d725e256fb432f1938de7367daa98d7d88d66c
shustov_v@bi.group <shustov_v@bi.group>
2022-07-15 17:32:03 +0600

    refactor code

--- app/src/main/java/bi/group/nsi_app/adapter/ItemAdapter.kt
+++ app/src/main/java/bi/group/nsi_app/adapter/ItemAdapter.kt
@@ -1,8 +1,37 @@
 package bi.group.nsi_app.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import bi.group.nsi_app.R
 import bi.group.nsi_app.model.Affirmation
 
 class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
-    class ItemViewHolder()
+    class ItemAdapter(
+        private val context: Context,
+        private val dataset: List<Affirmation>
+    ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+        class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+            val textView: TextView = view.findViewById(R.id.item_title)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+            // create a new view
+            val adapterLayout = LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item, parent, false)
+
+            return ItemViewHolder(adapterLayout)
+        }
+
+        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+            val item = dataset[position]
+            holder.textView.text =  context.resources.getString(item.stringResourceId)
+        }
+
+        override fun getItemCount() = dataset.size
+    }
 }
