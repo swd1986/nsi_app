801b5f09ae257b665e74d2dc02269f857f463782
shustov_v@bi.group <shustov_v@bi.group>
2022-07-15 17:17:08 +0600

    refactor code

--- app/src/main/java/bi/group/nsi_app/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/bi/group/nsi_app/ui/gallery/GalleryFragment.kt
@@ -28,10 +28,11 @@ class GalleryFragment : Fragment() {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        //val textView: TextView = binding.textGallery
+
+//        galleryViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
