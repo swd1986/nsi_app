c49a7c8ff1f13e4038481c0e8915366bc8965e43
shustov_v@bi.group <shustov_v@bi.group>
2022-07-15 17:27:12 +0600

    refactor code

---
+++ app/src/main/java/bi/group/nsi_app/adapter/ItemAdapter.kt
@@ -0,0 +1,8 @@
+package bi.group.nsi_app.adapter
+
+import android.content.Context
+import bi.group.nsi_app.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
+    class ItemViewHolder()
+}
